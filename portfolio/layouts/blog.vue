de09b4542621a45e9b4b46e8f909d3c167622241
adhiyudanas <adhiyudanas@gmail.com>
2022-01-31 23:11:25 +0900

    add additional components

---
+++ portfolio/layouts/blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>My blog navigation bar here</div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'blog'
+    // page component definitions
+  }
+</script>
--- portfolio/pages/index.vue
+++ portfolio/pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>This is index</h1>
+  <h1 class="red">Hello World</h1>
 </template>
+
+<script>
+  export default {
+    head() {
+      // Set Meta Tags for this Page
+    }
+    // ...
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
